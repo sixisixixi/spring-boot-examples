b9382be661361a1a2c1442d741cd327c847be59a
sixisixixi <148740650+sixisixixi@users.noreply.github.com>
2023-11-23 13:20:14 +0800

    Add files via upload

---
+++ springboot/src/main/java/com/example/springboot/controller/TestServer.java
@@ -0,0 +1,14 @@
+package com.example.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestServer {
+    @ResponseBody
+    @RequestMapping("test")
+    public String test(){
+        return "hello springboot";
+    }
+}
